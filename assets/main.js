9c1aa71c0c10a16ad4a4b42135e080091436ae90
300ms <ilhansnmz44@gmail.com>
2020-11-06 20:50:09 +0300

    Add local storage functions

--- assets/main.js
+++ assets/main.js
@@ -1,56 +1,80 @@
-let myBookStore = [
-  {
-    author: 'Author one',
-    title: 'Book 1',
-    pages: '300'
-  },
-
-  {
-    author: 'Author two',
-    title: 'Book 2',
-    pages: '150'
+class Book{
+  constructor(author, title, pages) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
   }
-]
+};
 
-function Book(author, title, pages){
-  this.author = author;
-  this.title = title;
-  this.pages = pages;
+class Store{
+  static getBooksFromLocal() {
+    let books;
+    if (localStorage.getItem('books') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
+    }
+
+    return books;
+  };
+
+  static addBookToLocal(book) {
+    const books = Store.getBooksFromLocal();
+    books.push(book);
+
+    localStorage.setItem('books', JSON.stringify(books));
+  };
+
+  static removeBookFromLocal(title, author, pages) {
+    const books = Store.getBooksFromLocal();
+
+    books.forEach((b, index) => {
+      if (b.title === title && b.author === author && b.pages === pages) {
+        books.splice(index, 1);
+      }
+    });
+
+    localStorage.setItem('books', JSON.stringify(books));
+  };
 }
 
-function displayBooks() {
-  myBookStore.forEach((book) => {
-    addBook(book)
-  });
-}
+class UI {
+  static displayBooks() {
+    const myBookStore = Store.getBooksFromLocal();
 
-function addBook(book){
-  let table = document.querySelector('#book-list');
-  let row = document.createElement('tr');
-
-  row.innerHTML = `
-    <td>${book.author}</td>
-    <td>${book.title}</td>
-    <td>${book.pages}</td>
-    <td><button class ="btn btn-info read">Unread</btn></td>
-    <td><button class ="btn btn-danger delete">X</btn></td>
-  `;
-
-  table.appendChild(row);
-}
-
-function removeBook(element) {
-  if(element.classList.contains("delete")) {
-    element.parentElement.parentElement.remove();
+    myBookStore.forEach((book) => {
+      UI.addBook(book);
+    });
+  };
+  
+  static addBook(book){
+    let table = document.querySelector('#book-list');
+    let row = document.createElement('tr');
+  
+    row.innerHTML = `
+      <td>${book.author}</td>
+      <td>${book.title}</td>
+      <td>${book.pages}</td>
+      <td><button class ="btn btn-info read">Unread</btn></td>
+      <td><button class ="btn btn-danger delete">X</btn></td>
+    `;
+  
+    table.appendChild(row);
+  };
+  
+  static removeBook(element) {
+    if(element.classList.contains("delete")) {
+      element.parentElement.parentElement.remove();
+    }
+  };
+  
+  static toggleClasses(){
+    document.getElementById('book-form').classList.toggle('d-none');
+    document.getElementById('hidden').classList.toggle('d-none');
   }
-}
+};
 
-function toggleClasses(){
-  document.getElementById('book-form').classList.toggle('d-none');
-  document.getElementById('hidden').classList.toggle('d-none');
-}
-
-document.addEventListener('DOMContentLoaded', displayBooks());
+document.addEventListener('DOMContentLoaded', UI.displayBooks());
 
 document.getElementById('book-form').addEventListener('submit', e => {
   e.preventDefault();
@@ -60,17 +84,28 @@ document.getElementById('book-form').addEventListener('submit', e => {
 
   const book = new Book(author, title, pages);
 
-  addBook(book);
-
-  toggleClasses();
+  UI.addBook(book);
+  Store.addBookToLocal(book);
+  UI.toggleClasses();
 });
 
 document.getElementById('hidden').addEventListener('click', e => {
-  toggleClasses();
+  UI.toggleClasses();
 });
 
 document.getElementById('book-list').addEventListener('click', e => {
-  removeBook(e.target);
+  element = e.target;
+
+  UI.removeBook(element);
+
+  if(element.classList.contains('delete')){
+    const pre = e.target.parentElement.previousElementSibling.previousElementSibling;
+    const pages = pre.textContent;
+    const title = pre.previousElementSibling.textContent;
+    const author = pre.previousElementSibling.previousElementSibling.textContent;
+    Store.removeBookFromLocal(title, author, pages);
+  }
+
 });
 
 document.getElementById('book-list').addEventListener('click', e => {
@@ -83,5 +118,4 @@ document.getElementById('book-list').addEventListener('click', e => {
       element.innerHTML = 'Unread';
     }
   }
-
 });
