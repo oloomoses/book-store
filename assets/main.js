3b3f77e09360d06f7ee8c4ea72b3d20bdce7dc1b
oloomoses <olooine@gmail.com>
2020-11-11 23:04:13 +0300

    refactor UI class

--- assets/main.js
+++ assets/main.js
@@ -45,6 +45,65 @@ class BookStorage {
 
 }
 
+class UI {
+
+  static addBook(book) {
+    const table = document.querySelector('#book-list');
+    const row = document.createElement('tr');
+    const {
+      author, title, pages, read,
+    } = book;
+
+    row.innerHTML = `
+      <td>${author}</td>
+      <td>${title}</td>
+      <td>${pages}</td>
+      <td><button class ="btn btn-info read">${read}</button></td>
+      <td><button class ="btn btn-danger delete">X</button></td>
+    `;
+
+    table.appendChild(row);
+  }
+
+  static displayBooks() {
+    const myBookStore = BookStorage.getBooksFromLocal();
+
+    myBookStore.forEach((book) => {
+      UI.addBook(book);
+    });
+  }
+
+  static showMsg(msg, className) {
+    const div = document.createElement('div');
+    div.className = `alert alert-${className}`;
+    div.appendChild(document.createTextNode(msg));
+
+    const header = document.querySelector('.container');
+    const content = document.querySelector('.content');
+
+    header.insertBefore(div, content);
+
+    setTimeout(() => document.querySelector('.alert').remove(), 1000);
+  }
+
+  static formReset() {
+    document.querySelector('#author').value = '';
+    document.querySelector('#title').value = '';
+    document.querySelector('#pages').value = '';
+    document.querySelector('#read').value = '';
+  }
+
+  static removeBook(element) {
+    if (element.classList.contains('delete')) {
+      element.parentElement.parentElement.remove();
+    }
+  }
+
+  static toggleClasses() {
+    document.getElementById('book-form').classList.toggle('d-none');
+    document.getElementById('hidden').classList.toggle('d-none');
+  }
+}
 
 
 document.addEventListener('DOMContentLoaded', UI.displayBooks());
