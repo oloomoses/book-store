556824577543da278ce81b11e195542b548c0586
oloomoses <olooine@gmail.com>
2020-11-11 23:02:52 +0300

    refactor to storage class

--- assets/main.js
+++ assets/main.js
@@ -7,97 +7,47 @@ class Book {
   }
 }
 
-const getBooksFromLocal = () => JSON.parse(localStorage.getItem('books')) || [];
 
-function addBookToLocal(book) {
-  const books = getBooksFromLocal();
-  books.push(book);
+class BookStorage {
 
-  localStorage.setItem('books', JSON.stringify(books));
-}
+  static getBooksFromLocal = () => JSON.parse(localStorage.getItem('books')) || [];
 
-function findBook(title, author, pages) {
-  const books = getBooksFromLocal();
-  let i = -1;
+  static addBookToLocal(book) {
+    const books = BookStorage.getBooksFromLocal();
+    books.push(book);
 
-  books.forEach((b, index) => {
-    if (b.title === title && b.author === author && b.pages === pages) {
-      i = index;
-    }
-  });
-
-  return i;
-}
-
-function removeBookFromLocal(title) {
-  const books = getBooksFromLocal();
-
-  books.forEach((book, index) => {
-    if (book.title === title) {
-      books.splice(index, 1);
-    }
-  });
-  localStorage.setItem('books', JSON.stringify(books));
-}
-
-function addBook(book) {
-  const table = document.querySelector('#book-list');
-  const row = document.createElement('tr');
-  const {
-    author, title, pages, read,
-  } = book;
-
-  row.innerHTML = `
-    <td>${author}</td>
-    <td>${title}</td>
-    <td>${pages}</td>
-    <td><button class ="btn btn-info read">${read}</button></td>
-    <td><button class ="btn btn-danger delete">X</button></td>
-  `;
-
-  table.appendChild(row);
-}
-
-function displayBooks() {
-  const myBookStore = getBooksFromLocal();
-
-  myBookStore.forEach((book) => {
-    addBook(book);
-  });
-}
-
-function showMsg(msg, className) {
-  const div = document.createElement('div');
-  div.className = `alert alert-${className}`;
-  div.appendChild(document.createTextNode(msg));
-
-  const header = document.querySelector('.container');
-  const content = document.querySelector('.content');
-
-  header.insertBefore(div, content);
-
-  setTimeout(() => document.querySelector('.alert').remove(), 1000);
-}
-
-function formReset() {
-  document.querySelector('#author').value = '';
-  document.querySelector('#title').value = '';
-  document.querySelector('#pages').value = '';
-  document.querySelector('#read').value = '';
-}
-
-function removeBook(element) {
-  if (element.classList.contains('delete')) {
-    element.parentElement.parentElement.remove();
+    localStorage.setItem('books', JSON.stringify(books));
   }
+
+  static findBook(title, author, pages) {
+    const books = BookStorage.getBooksFromLocal();
+    let i = -1;
+
+    books.forEach((b, index) => {
+      if (b.title === title && b.author === author && b.pages === pages) {
+        i = index;
+      }
+    });
+
+    return i;
+  }
+
+  static removeBookFromLocal(title) {
+    const books = BookStorage.getBooksFromLocal();
+
+    books.forEach((book, index) => {
+      if (book.title === title) {
+        books.splice(index, 1);
+      }
+    });
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+
 }
 
-function toggleClasses() {
-  document.getElementById('book-form').classList.toggle('d-none');
-  document.getElementById('hidden').classList.toggle('d-none');
-}
 
-document.addEventListener('DOMContentLoaded', displayBooks());
+
+document.addEventListener('DOMContentLoaded', UI.displayBooks());
 
 document.getElementById('book-form').addEventListener('submit', e => {
   e.preventDefault();
@@ -111,26 +61,26 @@ document.getElementById('book-form').addEventListener('submit', e => {
   } else {
     const book = new Book(author, title, pages, read);
 
-    addBook(book);
-    addBookToLocal(book);
-    formReset();
-    toggleClasses();
+    UI.addBook(book);
+    BookStorage.addBookToLocal(book);
+    UI.formReset();
+    UI.toggleClasses();
   }
 });
 
 document.getElementById('hidden').addEventListener('click', () => {
-  toggleClasses();
+  UI.toggleClasses();
 });
 
 document.getElementById('book-list').addEventListener('click', e => {
   const element = e.target;
 
-  removeBook(element);
+  UI.removeBook(element);
 
   if (element.classList.contains('delete')) {
     const pre = element.parentElement.previousElementSibling;
     const title = pre.previousElementSibling.previousElementSibling.textContent;
-    removeBookFromLocal(title);
+    BookStorage.removeBookFromLocal(title);
   }
 });
 
@@ -143,8 +93,8 @@ document.getElementById('book-list').addEventListener('click', e => {
     const title = pre.previousElementSibling.textContent;
     const author = pre.previousElementSibling.previousElementSibling.textContent;
 
-    const books = getBooksFromLocal();
-    const index = findBook(title, author, pages);
+    const books = BookStorage.getBooksFromLocal();
+    const index = BookStorage.findBook(title, author, pages);
 
     if (index >= 0) {
       if (element.innerHTML === 'Unread') {
